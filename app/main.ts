570517e9efbaf97c8f369e5c4d0b993d3665cd71
Saiteja Varma <saitejavarmaj30@gmail.com>
2024-12-05 00:57:09 +0530

    adding the response to the socket

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+
+  socket.on("data", (data) =>{
+    console.log("recieved the request", data.toString())
+
+    const httpResponse: string = `HTTP/1.1 200 OK\r\n\r\n`;
+    socket.write(httpResponse);
+    socket.end();
+
+  })
+
   socket.on("close", () => {
     socket.end();
   });
