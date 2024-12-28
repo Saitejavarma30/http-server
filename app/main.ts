b176f42e1a4327dc65decd18809c6d91e4b90ae2
Saiteja Varma <saitejavarmaj30@gmail.com>
2024-12-28 20:14:59 +0530

    making file read changes

--- app/main.ts
+++ app/main.ts
@@ -5,39 +5,35 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-
-  socket.on("data", (data) =>{
+  socket.on("data", (data) => {
     // console.log("recieved the request", data.toString())
-
 
     const dataAsString = data.toString();
     // console.log(dataAsString);
-    const tempData = dataAsString.split(" ")
-    const dynamic_val = tempData[1].split("/")[2]
-    if(tempData[1] === "/"){
+    const tempData = dataAsString.split(" ");
+    const dynamic_val = tempData[1].split("/")[2];
+    if (tempData[1] === "/") {
       const httpResponse: string = `HTTP/1.1 200 OK\r\n\r\n`;
       socket.write(httpResponse);
-    }
-    else if(tempData[1] === `/echo/${dynamic_val}`){
-        const httpResponse: string = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${dynamic_val.length}\r\n\r\n${dynamic_val}`
-        socket.write(httpResponse);
-      }
-    else if(tempData[1] === '/user-agent'){
-      const userAgent = dataAsString.split("\r\n")[2]
-      const userAgentFinal = userAgent.split(": ")[1]
-      console.log(userAgentFinal)
-      console.log(userAgentFinal.length)
-      const httpResponse: string = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentFinal.length}\r\n\r\n${userAgentFinal}`
+    } else if (tempData[1] === `/echo/${dynamic_val}`) {
+      const httpResponse: string = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${dynamic_val.length}\r\n\r\n${dynamic_val}`;
+      socket.write(httpResponse);
+    } else if (tempData[1] === "/user-agent") {
+      const userAgent = dataAsString.split("\r\n")[2];
+      const userAgentFinal = userAgent.split(": ")[1];
+      console.log(userAgentFinal);
+      console.log(userAgentFinal.length);
+      const httpResponse: string = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentFinal.length}\r\n\r\n${userAgentFinal}`;
+      socket.write(httpResponse);
+    } else if (tempData[1] === `/files/${dynamic_val}`) {
+      const httpResponse: string = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${dynamic_val.length}\r\n\r\n${dynamic_val}`;
+      socket.write(httpResponse);
+    } else {
+      const httpResponse: string = `HTTP/1.1 404 Not Found\r\n\r\n`;
       socket.write(httpResponse);
     }
-      else {
-        const httpResponse: string = `HTTP/1.1 404 Not Found\r\n\r\n`;
-        socket.write(httpResponse)
-    }
-   socket.end();
-
-
-  })
+    socket.end();
+  });
 
   socket.on("close", () => {
     socket.end();
